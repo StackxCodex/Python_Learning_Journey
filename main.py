ffd6dd2f94d8424b7d0ed5aeb7f17237bf185dbb
StackxCodex <siennastackx@gmail.com>
2026-05-21 09:12:39 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+name = "Shelly a.k.a. StackxCodex"
+print("Hello, my name is " + name)
+print()
+School = "Maestro University"
+print("I'm currently an AI software engineer student at " + School + ".")
+print("" + School + " has giving me an opportunity to return to school after 20 years.")
+print("I am currently learning Python and how to use GitHub.")
