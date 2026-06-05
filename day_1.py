0118cc000324f0b4fffce89232dceff676333633
StackxCodex <siennastackx@gmail.com>
2026-06-04 18:41:18 -0700

    day_1

---
+++ day_1.py
@@ -0,0 +1,24 @@
+#Introduction
+# Day 1 -30 Days of Python Challenge
+print()
+print("Hello World!\tI am learning Python.")
+print()
+print(2+3) # addition(+)
+print(3-1) # subtraction(-)
+print(2*3) # multiplication(*)
+print(3+2) # addition(+)
+print(3*2) # multiplication(*)
+print(3/2) # division(/)
+print(3 ** 2) # exponentiation(**)
+print(3 % 2) # modulus(%)
+print(3 // 2) # floor division(//)
+print()
+# Chechking data types
+print()
+print(type(10)) # int
+print(type(3.14)) # float
+print(type(1 + 3j)) # complex
+print(type("Stackx")) # str
+print(type([1, 2, 3, 4])) # list
+print(type({'name':'Stackx', 'age': 38 })) # dict
+print(type((9.8, 3.14, 2.7, 4))) # tuple
