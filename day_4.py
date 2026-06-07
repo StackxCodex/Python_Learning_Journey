eacd096e53d4add33645dbf5971c925e9c9742b7
StackxCodex <siennastackx@gmail.com>
2026-06-07 11:13:15 -0700

    Day_4

--- day_4.py
+++ day_4.py
@@ -58,7 +58,7 @@ language + "Python"
 last_letter = language[-1]
 print(last_letter)
 second_last = language[-2]
-print(sond_last)
+print(second_last)
 
 #Slicing
 language = 'Python'
@@ -76,4 +76,58 @@ print(pto)
 print(' hope everyone is enjoying the python challenge.\n Do you?')
 print('Days\tTopics\tExercises')
 print('Day 1\t3\t5')
-')
+print('Day 2\t3\t5')
+print('Day 3\t3\t5') 
+print('Day 4\t3\t5')
+print('This is a back slash symbol (\\)')
+print('In every pragmming language it starts with \"Hello, World!\"')
+
+# String Methods
+# capitalization(): Converts the first character of the string into Capital letter
+challenge = 'thirty days of python'
+print(challenge.capitalize())
+
+# count(): returns occurences of substring in a string, count(subsstring, start=.., end=..)
+chanllenge = 'thirty days of python'
+print(challenge.count('y'))
+print(challenge.count('y', 7, 14))
+print(challenge.count('th'))
+
+# endswith(): Checks if a string ends with a specified ending 
+challenge = 'thirty days of python'
+print(challenge.endwith('on'))
+print(challenge.endwith('tion'))
+
+# expandtabs(): Replaces tab character with spaces, default tab size is 8. It takes tab size argument 
+challenge = 'thirty\tdays\yof\tpython'
+print(challenge.expandtabs())
+print(challenge.expandtabs(10))
+
+# find(): Return the index of the first occurence of substring 
+challenge(challenge.find('y'))
+print(challenge.find('th'))
+
+# format() Formats string into nicer output 
+first_name = 'Stackx'
+last_name = 'Codex'
+job = 'Salon Operator and Braider'
+country = 'USA'
+sentence = 'I am {} {}. I am a {}. I live in {}. '.format(
+    first_name ,last_name, job, country)
+print(sentence)
+
+radius = 10
+pi = 3.14 
+area = pi # radius ## 2
+result = 'The area of a circle with {} is {}'.format(str(radius), str(area))
+print(result)
+
+#indexing(): Returns the index of substring 
+challenge ='thirty days of python'
+print(challenge.find('y'))
+print(challenge.find('th'))
+
+# isalnum(): Checks alphanumeric characters 
+challenge = 'ThityDaysofPython' 
+print(challenge.isalnum())
+
