e240d437858d7574b20a6269a9ceed3346864b3b
StackxCodex <siennastackx@gmail.com>
2026-06-10 12:38:26 -0700

    todo_list

---
+++ todo_list.py
@@ -0,0 +1,34 @@
+def to_do_list():
+    tasks = []
+
+    while True:
+        print("1. Add task")
+        print("2. Remove task")
+        print("3. Show tasks")
+        print("4. Quit")
+        choice = input("Enter your choice")
+
+        if choice == "1":
+            task = input("Enter task: ")
+            task.append(task)
+        elif choice == "2":
+            task = input("Enter task to remove")
+            if task in tasks:
+                task.remove(task)
+            else:
+                print("Task not found")
+        elif choice == ("3"):
+            print("Task: ")
+            for task in tasks:
+                print("- " + task)
+        elif choice ==("4")
+            break
+        else:
+            print("invalid choice")
+
+to_do_list()
+
+
+
+
+
